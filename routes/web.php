57e034695de9b0d4ef130ba5d39f396378b7bea6
indritanialestari <indritania3@gmail.com>
2024-10-31 10:09:16 +0700

    video 3 - Struktur Folder

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Indri Tania Lestari",
+        "email" => "Indri.22304009@mail.ac.unpad.id",
+        "image" => "dokja.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
