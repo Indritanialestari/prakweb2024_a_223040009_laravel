d001d9c8474b1bf1adcb18240beea13997e331a1
indritanialestari <indritania3@gmail.com>
2024-11-02 00:01:01 +0700

    video 9 - Eloquent ORM & Post Model

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,25 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['name'=>'Indri Tania Lestari', 'title' => 'About']);
+    return view('about', ['name'=>'Rafli Ramdhani', 'title' => 'About']);
 });
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function($slug){
-    $post = Post::find($slug);
+Route::get('/posts/{post:slug}', function(Post $post){
+
+    // $post = Post::find($slug);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
 });
