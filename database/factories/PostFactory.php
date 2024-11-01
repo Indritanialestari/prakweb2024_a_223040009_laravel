4811b3e0f92cd422f35c3c060c4df69586d123da
indritanialestari <indritania3@gmail.com>
2024-11-02 01:21:50 +0700

    video 11 - Eloquent Relationship

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,7 +1,11 @@
 <?php
+
 namespace Database\Factories;
+
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\Factory;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
@@ -16,7 +20,7 @@ class PostFactory extends Factory
     {
         return [
             'title' => fake()->sentence(10, false),
-            'author' => fake()->name(),
+            'author_id' => User::factory(),
             'slug' => Str::slug(fake()->sentence()),
             'body' => fake()->text(),
         ];
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,18 +11,20 @@ Route::get('/', function () {
 });
 
 Route::get('/about', function () {
-    return view('about', ['name'=>'Rafli Ramdhani', 'title' => 'About']);
+    return view('about', ['name'=>'Indri Tania Lestari', 'title' => 'About']);
 });
 Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
 Route::get('/posts/{post:slug}', function(Post $post){
-
     // $post = Post::find($slug);
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
-
+Route::get('/authors/{user}', function(User $user){
+    // $post = Post::find($slug);
+    return view('posts', ['title' => 'Articles By ' . $user->name, 'posts' => $user->posts]);
+});
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });
