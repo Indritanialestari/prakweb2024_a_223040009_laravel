46f2fe7ba5010ef68e302897bc77c7a18a5721e7
indritanialestari <indritania3@gmail.com>
2024-11-01 16:40:10 +0700

    video 4 - Blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,14 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman About</title>
     <link rel="stylesheet" href="css/style.css">
-    <title>WPU Blog | About</title>
 </head>
 <body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">About</a>
+    <a href="/contact">About</a>
     <h1>Halaman About</h1>
-    <h3><?= $name; ?></h3>
-    <p><?= $email; ?></p>
-    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200">
+    <h3>Owener : {{$nama}}</h3>
+    <img src="img/senku.jpg" alt="" width="200">
 </body>
 </html>
